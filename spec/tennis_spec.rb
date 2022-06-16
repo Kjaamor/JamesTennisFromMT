84a3b98fdb1f7ec2553de7844fe48ce66311ca92
James O'Keefe <james.okeefe@Jamess-MBP.fritz.box>
2022-06-16 14:52:57 +0100

    Tests added after session with Sam

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,8 +1,17 @@
 require_relative '../tennis'
 
 describe 'Tennis Scoreboard' do
-    it 'Player1 has 0 points' do
-        expect(player1_score(0)).to eq("0")
+    it 'Displays 0 - 0 at the start' do
+        expect(scoreboard(0,0)).to eq("0 - 0")
+    end
+    it 'Displays 15-0 if player 1 scores first' do
+        expect(scoreboard(15,0)).to eq("15 - 0")
+    end
+    it 'Displays Deuce if both players have 40 points' do
+        expect(scoreboard(40,40)).to eq("Deuce")
+    end
+    it 'Displays "Player 1 wins" if Player 1 passes 40 points & Player 2 has 30 points or fewer' do
+        expect(scoreboard("A",30)).to eq("Player 1 wins")
     end
 end
 
--- tennis.rb
+++ tennis.rb
@@ -1,6 +1,20 @@
 points = Array["0","15","30","40","A","Game"]
+=begin
+player1_score = 0..5
+player2_score = 0
+score_display = "0 - 0"
+=end
+def scoreboard (player1_score, player2_score)
+    if player1_score == "A" && player2_score <31
+        return "Player 1 wins"
+    elsif player1_score == 40 && player2_score == 40
+        return "Deuce"
+    else
+    return player1_score.to_s + " - " + player2_score.to_s
+    print score_display
+    end
+end
 
-def player1_score(tennis_score)
-    points[0]
+def playerscores ()
 end
 
